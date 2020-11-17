a80f9841436f20838c1f75fa14103fbefd8de6a8
tusharnankani <tusharnankani3@gmail.com>
2020-11-17 21:38:22 +0530

    :hammer: refactor: minor change in layout, change delay

--- tree_visualization.c
+++ tree_visualization.c
@@ -11,26 +11,26 @@ void printTree(int x, int y, int* array,int index,int total_elements)
     char str[10];
     //Base case of our recursive function
     if (index >= total_elements)
-	return;
+	    return;
 
     // Convert int value into string
     itoa(array[index],str,10);
-    // Set color of the boundary of
-    // circle as green
-    setcolor(GREEN);
-    // Draw the circle of radius 15
-    // that represent node of Tree
+
+    // Set color of the boundary of circle
+    setcolor(BLACK);
+
+    // Draw the circle of radius 15 that represent node of Tree
     circle(x, y, 15);
-    delay(100);
-    floodfill(x, y, GREEN);
-    delay(100);
+    delay(200);
+    floodfill(x, y, RED);
+    delay(200);
+
     // Print the values of the node
     // in the circle
     outtextxy(x - 2, y - 3, str);
-    delay(100);
+    delay(200);
     // Set the color of the line
-    // from parent to child as green
-    setcolor(GREEN);
+    setcolor(RED);
     // Evaluating left and right child
     left = 2 * index + 1;
     right = 2 * index + 2;
@@ -38,19 +38,19 @@ void printTree(int x, int y, int* array,int index,int total_elements)
     // and the right subtree
     printTree(x - y / (index + 1), y + 50,array, left, total_elements);
     printTree(x + y / (index + 1), y + 50,array, right, total_elements);
+    
     // Draw the line (or link) when the
     // node is not the leaf node
     if (left < total_elements)
     {
-	delay(100);
-	line(x, y, x - y / (index + 1), y + 50);
+        delay(200);
+	    line(x, y + 14, x - y / (index + 1), y + 50 - 14);
     }
     if (right < total_elements)
     {
-	delay(100);
-	line(x, y, x + y / (index + 1), y + 50);
+        delay(200);
+        line(x, y + 14, x + y / (index + 1), y + 50 - 14);
     }
-    return ;
 }
 
 int main()
@@ -63,10 +63,11 @@ int main()
     array=(int*)malloc(sizeof(int)*size);
     for(i=0;i<size;++i)
     {
-	printf("Enter node %d: ",i+1);
-	scanf("%d",(array+i));
+        printf("Enter node %d: ",i+1);
+        scanf("%d",(array+i));
     }
     clrscr();
+    setbkcolor(BLACK);
     printTree(300, 100, array, 0, size);
     getch();
     closegraph();
